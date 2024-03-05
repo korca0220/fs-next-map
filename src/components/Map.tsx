3c178753500b3c66a3046fe582636a192e55c3de
ParkJuneWoo <krca0220@gmail.com>
2024-03-05 22:38:13 +0900

    add marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,6 @@
 /*global kakao */
 
+import * as stores from "@/data/store_data.json";
 import Script from "next/script";
 
 declare global {
@@ -8,28 +9,44 @@ declare global {
   }
 }
 
+const DEFAULT_LAT = 37.497625203;
+const DEFAULT_LNG = 127.03088379;
+
 export default function Map() {
-    const loadKakaoMap = () => {
-        window.kakao.maps.load(() => {
-          const mapContainer = document.getElementById("map");
-          const mapOption = {
-            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3,
-          };
-    
-          new window.kakao.maps.Map(mapContainer, mapOption);
-        });
+  const loadKakaoMap = () => {
+    window.kakao.maps.load(() => {
+      const mapContainer = document.getElementById("map");
+      const mapOption = {
+        center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
+        level: 3,
       };
-    
-      return (
-        <>
-          <Script
-            strategy="afterInteractive"
-            type="text/javascript"
-            src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
-            onReady={loadKakaoMap}
-          />
-          <div id="map" className="w-full h-screen"></div>
-        </>
-      );
+
+      const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+      stores?.["DATA"]?.map((store) => {
+        var markerPosition = new window.kakao.maps.LatLng(
+          store?.y_dnts,
+          store?.x_cnts
+        );
+
+        var marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+        });
+
+        marker.setMap(map);
+      });
+    });
+  };
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
+        onReady={loadKakaoMap}
+      />
+      <div id="map" className="w-full h-screen"></div>
+    </>
+  );
 }
