4bb432ae9f32d6d34ef0564f68b03142ca8d06ce
ParkJuneWoo <krca0220@gmail.com>
2024-03-24 22:58:41 +0900

    add selected restaurant ui

--- src/components/Markers.tsx
+++ src/components/Markers.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Dispatch, SetStateAction, useCallback, useEffect } from "react";
 
 interface MarkerProps {
--- src/components/StoreBox.tsx
+++ src/components/StoreBox.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 import { Dispatch, SetStateAction } from "react";
+import {
+  AiOutlineCheck,
+  AiOutlineClose,
+  AiOutlineInfoCircle,
+  AiOutlinePhone,
+} from "react-icons/ai";
+import { HiOutlineMapPin } from "react-icons/hi2";
 
 interface StoreBoxProps {
   store: any;
@@ -10,18 +17,52 @@ export default function StoreBox({ store, setStore }: StoreBoxProps) {
     <div className="fixed transition ease-in-out delay-150 inset-x-0 mx-auto bottom-20 rounded-lg shadow-lg max-w-sm md:max-w-xl z-10 w-full bg-white">
       {store && (
         <>
-          <div>
-            <Image
-              src={
-                store?.bizcnd_code_nm
-                  ? `/images/markers/${store?.bizcnd_code_nm}.png`
-                  : "/images/markers/default.png"
-              }
-              width={100}
-              height={100}
-              alt="아이콘 이미지"
-            />
+          <div className="p-8">
+            <div className="flex justify-between items-start">
+              <div className="flex gap-4 items-center">
+                <Image
+                  src={
+                    store?.bizcnd_code_nm
+                      ? `/images/markers/${store?.bizcnd_code_nm}.png`
+                      : "/images/markers/default.png"
+                  }
+                  width={40}
+                  height={40}
+                  alt="아이콘 이미지"
+                />
+                <div>
+                  <div className="font-semibold ">{store?.upso_nm}</div>
+                  <div className="font-sm ">{store?.cob_code_nm}</div>
+                </div>
+              </div>
+              <button type="button" onClick={() => setStore(null)}>
+                {<AiOutlineClose />}
+              </button>
+            </div>
+            <div className="mt-2 flex gap-2 items-center">
+              <HiOutlineMapPin />
+              {store?.rdn_code_nm}
+            </div>
+            <div className="mt-2 flex gap-2 items-center">
+              <AiOutlinePhone />
+              {store?.tel_no}
+            </div>
+            <div className="mt-2 flex gap-2 items-center">
+              <AiOutlineInfoCircle />
+              {store?.crtfc_gbn_nm}
+            </div>
+            <div className="mt-2 flex gap-2 items-center">
+              <AiOutlineCheck />
+              {store?.bizcnd_code_nm}
+            </div>
           </div>
+          <button
+            type="button"
+            onClick={() => window.alert("상세보기 작업중")}
+            className="w-full py-3 text-white font-semibold  bg-blue-700 hover:bg-blue-500 rounded-b-lg"
+          >
+            상세보기
+          </button>
         </>
       )}
     </div>
